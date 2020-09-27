80c575fa8a2fc56d02123ba33880915754dfb6e8
Ernsyka Methelus <ernsykamethelus@yahoo.com>
2020-09-27 03:16:11 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -14,5 +14,11 @@ class Transfer
   
   def execute_transaction
     if valid? && sender.balance >> amount && self.status == "pending"
+      sender.withdrawal(self.amount)
+      receiver.deposit(self.amount)
+      self.status = "complete"
+    else
+      self.status = "rejected"
+      
     end
 end
