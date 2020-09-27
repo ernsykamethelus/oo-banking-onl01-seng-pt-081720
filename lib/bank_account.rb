fa5b7f4378636859623d4df240c995df3a887a2d
Ernsyka Methelus <ernsykamethelus@yahoo.com>
2020-09-27 03:03:24 +0000

    Automatically backed up by Learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -2,7 +2,9 @@ class BankAccount
   attr_accessor :balance, :status
   attr_reader :name
   def initialize (name, balance = 1000, status="open")
-    
+    @name =name
+    @balance = balance
+    @status = status
   end
 
 end
