869252bebee784463e4634618fc80b3e8fbca6e3
Ernsyka Methelus <ernsykamethelus@yahoo.com>
2020-09-27 03:18:07 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -24,7 +24,15 @@ class Transfer
   end
   
   def reverse_transfer
-    
+     def reverse_transfer
+    if valid? && receiver.balance > amount && self.status == "complete"
+      sender.deposit(self.amount)
+      receiver.withdrawal(self.amount)
+      self.status = "reversed"
+    else
+      self.status = "rejected"
+      "Transaction rejected. Please check your account balance."      
+    end
   end
   
 end
